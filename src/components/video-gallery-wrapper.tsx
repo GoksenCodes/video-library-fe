1de2d9d3b12a71cf414257a723929163709786fe
Goksen Gorgulu <goksengorgulu@gmail.com>
2025-04-18 00:33:31 +0200

    sync page and search state with URL using useSearchParams

--- src/components/video-gallery-wrapper.tsx
+++ src/components/video-gallery-wrapper.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { fetchVideos } from '../api/videos-api-client';
-import { useVideoQueryParams } from '../hooks/use-video-query-params';
 import { useVideoGalleryState } from '../hooks/use-video-gallery-state';
 import { renderVideoState } from '../utils/render-video-state';
 import SearchBar from './search-bar';
 import PaginationControls from './pagination-controls';
 import { PaginatedResponse } from '../types/paginated-response';
+import { useVideoSearchParams } from '../hooks/use-video-search-params';
 
 const VideoGalleryWrapper: React.FC = () => {
-  const { page, setPage, search, setSearch } = useVideoQueryParams();
+  const { page, setPage, search, setSearch } = useVideoSearchParams();
 
   const { data, isLoading, isError } = useQuery<PaginatedResponse>({
     queryKey: ['videos', { page, search }],
